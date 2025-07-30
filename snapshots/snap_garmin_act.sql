bf3e83ce27d784f8d822ff2251d8b086958ccaa1
Keith Evans <keith.evans@yesenergy.com>
2025-07-29 20:35:39 -0400

    Clean Up

--- snapshots/snap_garmin_act.sql
+++
@@ -1,7 +0,0 @@
-{{ 
-    config(materialized='ephemeral'
-    ,tags=["garmin"])
-}}
-
-select * 
-from {{ ref('land_garmin_activies') }}
--- snapshots/snap_strava_act_lap.sql
+++
@@ -1,7 +0,0 @@
-{{ 
-    config(materialized='ephemeral'
-    ,tags=["strava"])
-}}
-
-select *
-from {{ ref('land_strava_act_lap_raw') }}
